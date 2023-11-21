789964bedb2dd766f435a5f3c6c24cc1560bba05
Nader Farahani <usern@nadermac.home>
2023-11-21 17:19:25 +0000

    initial commit

--- src/routers/App.js
+++ src/routers/App.js
@@ -1,80 +1,138 @@
 import '../App.css';
-import Bats from '../components/Bats';
-import Cheetah from '../components/Cheetah';
-import Bears2 from '../components/Bears2';
-import Bears from '../components/Bears';
-import Bears3 from '../components/Bears3';
-import Bears4 from '../components/Bears4';
-import Animals from '../components/Animals';
-import Buffalo from '../components/Buffalo';
-import Camels from '../components/Camels';
-import Cats from '../components/Cats';
-import Chips from '../components/Chips';
-import Cows from '../components/Cows';
-import Doggo from '../components/Doggo';
-import Elephant from '../components/Elephant';
-import Giraffe from '../components/Giraffe';
-import Gorillaz from '../components/Gorillaz';
-import Gorillaz2 from '../components/Gorillaz2';
-import Gorillaz3 from '../components/Gorillaz3';
-import Hippos from '../components/Hippos';
-import Horses from '../components/Horses';
-import Lemurs from '../components/Lemurs';
-import Lionness from '../components/Lionness';
-import Lionness2 from '../components/Lionness2';
-import Lontra from '../components/Lontra';
-import Monkeys from '../components/Monkeys';
-import Owls from '../components/Owls';
-import Owls2 from '../components/Owls2';
-import Pandas from '../components/Pandas';
-import Penguins from '../components/Penguins';
-import PolarBear from '../components/PolarBear';
-import Squirrels from '../components/Squirrels';
-import Swans from '../components/Swans';
-import Tartoise from '../components/Tartoise';
-import WildDoggo from '../components/WildDoggo';
+import Bb1 from '../components/Bb1';
+import Bb2 from '../components/Bb2';
+import Bb3 from '../components/Bb3';
+import Bb4 from '../components/Bb4';
+import Bb5 from '../components/Bb5';
+import Bb6 from '../components/Bb6';
+import Bb7 from '../components/Bb7';
+import Bb8 from '../components/Bb8';
+import Bb9 from '../components/Bb9';
+import Bp1 from '../components/Bp1';
+import Bp2 from '../components/Bp2';
+import Bp3 from '../components/Bp3';
+import Kr1 from '../components/Kr1';
+import Kr2 from '../components/Kr2';
+import Kr3 from '../components/Kr3';
+import Kr4 from '../components/Kr4';
+import Kr5 from '../components/Kr5';
+import Pub from '../components/Pub';
+import Sjp1 from '../components/Sjp1';
+import Ms1 from '../components/Ms1';
+import Ms2 from '../components/Ms2';
+import Ms3 from '../components/Ms3';
+import Ms4 from '../components/Ms4';
+import Ms5 from '../components/Ms5';
+import Ms6 from '../components/Ms6';
+import Ms7 from '../components/Ms7';
+import Ms8 from '../components/Ms8';
+import Ms9 from '../components/Ms9';
+import Ms10 from '../components/Ms10';
+import Ms11 from '../components/Ms11';
+import Ms12 from '../components/Ms12';
+import Ms13 from '../components/Ms13';
+import Ms14 from '../components/Ms14';
+import Ms15 from '../components/Ms15';
+import Ms16 from '../components/Ms16';
+import Ms17 from '../components/Ms17';
+import Ms18 from '../components/Ms18';
+import Ms19 from '../components/Ms19';
+import Ms20 from '../components/Ms20';
+import K1 from '../components/K1';
+import K2 from '../components/K2';
+import K3 from '../components/K3';
+import K4 from '../components/K4';
+import K5 from '../components/K5';
+import K6 from '../components/K6';
+import K7 from '../components/K7';
+import K8 from '../components/K8';
+import N1 from '../components/N1';
+import N2 from '../components/N2';
+import N3 from '../components/N3';
+import S1 from '../components/S1';
+import S2 from '../components/S2';
+import S3 from '../components/S3';
+import S4 from '../components/S4';
+import S5 from '../components/S5';
+import S6 from '../components/S6';
+import S7 from '../components/S7';
+import S8 from '../components/S8';
+import S9 from '../components/S9';
+import SKY1 from '../components/SKY1';
+import T1 from '../components/T1';
+import T2 from '../components/T2';
+
 
 const App = () => {
   return (
     <div className="container">
         <div class="header">
-          <h1>The Mother</h1>
+          <h1>Kaori & Nader</h1>
         </div>
         <div id="gallery">
-          <Animals />
-          <Bats />
-          <Cheetah />
-          <Bears />
-          <Bears2 />
-          <Bears3 />
-          <Bears4 />
-          <Buffalo />
-          <Camels />
-          <Cats />
-          <Chips />
-          <Cows />
-          <Doggo />
-          <Elephant />
-          <Giraffe />
-          <Gorillaz />
-          <Gorillaz2 />
-          <Gorillaz3/>
-          <Hippos />
-          <Horses />
-          <Lemurs />
-          <Lionness />
-          <Lionness2 />
-          <Lontra />
-          <Monkeys />
-          <Owls />
-          <Owls2 />
-          <Pandas/>
-          <Penguins />
-          <PolarBear />
-          <Squirrels />
-          <Swans />
-          <Tartoise />
-          <WildDoggo />
+          <Bb1 />
+          <Bb2 />
+          <Bb3 />
+          <Bb4 />
+          <Bb5 />
+          <Bb6 />
+          <Bb7 />
+          <Bb8 />
+          <Bb9 />
+          <Bp1 />
+          <Bp2 />
+          <Bp3 />
+          <Kr1 />
+          <Kr2 />
+          <Kr3 />
+          <Kr4 />
+          <Kr5 />
+          <Pub />
+          <Sjp1 />
+          <Ms1 />
+          <Ms2 />
+          <Ms3 />
+          <Ms4 />
+          <Ms5 />
+          <Ms6 />
+          <Ms7 />
+          <Ms8 />
+          <Ms9 />
+          <Ms10 />
+          <Ms11 />
+          <Ms12 />
+          <Ms13 />
+          <Ms14 />
+          <Ms15 />
+          <Ms16 />
+          <Ms17 />
+          <Ms18 />
+          <Ms19 />
+          <Ms20 />
+          <K1 />
+          <K2 />
+          <K3 />
+          <K4 />
+          <K5 />
+          <K6 />
+          <K7 />
+          <K8 />
+          <N1 />
+          <N2 />
+          <N3 />
+          <S1 />
+          <S2 />
+          <S3 />
+          <S4 />
+          <S5 />
+          <S6 />
+          <S7 />
+          <S8 />
+          <S9 />
+          <SKY1 />
+          <T1 />
+          <T2 />
+
         </div>
     </div>
   )
